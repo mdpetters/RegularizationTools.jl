ec3a1f257a874338031be00195bff5c066c44c8a
Markus Petters <mdpetter@ncsu.edu>
2020-09-17 22:53:05 -0400

    add initial guess gcv

--- src/optimization.jl
+++ src/optimization.jl
@@ -9,10 +9,12 @@ function solve(
     λ₂ = 1000.0,
 )
     b̄ = @pipe b |> to_standard_form(Ψ, _)
+    L1, L2, κ  = Lcurve_functions(Ψ, b̄)
 
     λ = @match alg begin
         :gcv_tr => optimize(λ -> gcv_tr(Ψ, b̄, λ), λ₁, λ₂) |> Optim.minimizer
         :gcv_svd => optimize(λ -> gcv_svd(Ψ, b̄, λ), λ₁, λ₂) |> Optim.minimizer
+        :L_curve => optimize(λ -> 1.0-κ(λ), λ₁, λ₂) |> Optim.minimizer
         _ => throw("Unknown Algorithm")
     end
 
@@ -32,11 +34,12 @@ function solve(
     λ₂ = 1000.0,
 )
     b̄ = @pipe b |> to_standard_form(Ψ, _)
-    L1, L2, κ  = Lcurve_functions(Ψ, b̄, Ψ.L*x₀)
+    x̄₀ = Ψ.L * x₀
+    L1, L2, κ  = Lcurve_functions(Ψ, b̄, x̄₀)
 
     λ = @match alg begin
-        :gcv_tr  => optimize(λ -> gcv_tr(Ψ, b̄, λ), λ₁, λ₂)  |> Optim.minimizer
-        :gcv_svd => optimize(λ -> gcv_svd(Ψ, b̄, λ), λ₁, λ₂) |> Optim.minimizer
+        :gcv_tr  => optimize(λ -> gcv_tr(Ψ, b̄, x̄₀, λ), λ₁, λ₂)  |> Optim.minimizer
+        :gcv_svd => optimize(λ -> gcv_svd(Ψ, b̄, x̄₀, λ), λ₁, λ₂) |> Optim.minimizer
         :L_curve => optimize(λ -> 1.0-κ(λ), λ₁, λ₂) |> Optim.minimizer
         _ => throw("Unknown Algorithm")
     end
@@ -53,11 +56,19 @@ function gcv_tr(Ψ::RegularizationProblem, b::AbstractVector, λ::AbstractFloat)
     return n * norm((Ψ.Iₙ - Aλ) * b)^2.0 / tr(Ψ.Iₙ - Aλ)^2.0
 end
 
+function gcv_tr(Ψ::RegularizationProblem, b̄::AbstractVector, x̄₀::AbstractVector, λ::AbstractFloat)
+    n = size(Ψ.Ā, 1)
+    x̄(λ::AbstractFloat)   = solve(Ψ, b̄, x̄₀, λ) 
+    L1(λ::AbstractFloat)  = norm(Ψ.Ā * x̄(λ)  - b̄)
+    Aλ = Ψ.Ā * inv(Ψ.ĀĀ + λ^2.0 * Ψ.Iₚ) * Ψ.Ā'
+    return n * L1(λ)^2.0 / tr(Ψ.Iₙ - Aλ)^2.0
+end
+
 # SVD based algorithm to find gcv estimate
-function gcv_svd(Ψ::RegularizationProblem, b::AbstractVector, λ::AbstractFloat)
+function gcv_svd(Ψ::RegularizationProblem, b̄::AbstractVector, λ::AbstractFloat)
     F = Ψ.F̄
     n, p = size(F.U)
-    z = F.U' * b
+    z = F.U' * b̄
     nλ = λ^2.0
 
     s1 = 0.0
@@ -70,15 +81,51 @@ function gcv_svd(Ψ::RegularizationProblem, b::AbstractVector, λ::AbstractFloat
         s2 += nλ / (F.S[j] * F.S[j] + nλ)
     end
 
-    return n * (norm(b)^2.0 - norm(z)^2.0 + s1) / (n - p + s2)^2.0
+    return n * (norm(b̄)^2.0 - norm(z)^2.0 + s1) / (n - p + s2)^2.0
 end
+
+function gcv_svd(Ψ::RegularizationProblem, b̄::AbstractVector, x̄₀::AbstractVector, λ::AbstractFloat)
+    x̄(λ::AbstractFloat)   = solve(Ψ, b̄, x̄₀, λ) 
+    L1(λ::AbstractFloat)  = norm(Ψ.Ā * x̄(λ)  - b̄)
+
+    F = Ψ.F̄
+    n, p = size(F.U)
+    nλ = λ^2.0
+
+    s2 = 0.0
+    for j = 1:p
+        s2 += nλ / (F.S[j] * F.S[j] + nλ)
+    end
+
+    return n * L1(λ)^2.0 / (n - p + s2)^2.0
+end
+
 
 function Lcurve_functions(Ψ::RegularizationProblem, b̄::AbstractVector, x̄₀::AbstractVector)
     x̄(λ::AbstractFloat)   = solve(Ψ, b̄, x̄₀, λ) 
     L1(λ::AbstractFloat)  = norm(Ψ.Ā * x̄(λ)  - b̄)
     L2(λ::AbstractFloat)  = norm(Ψ.Iₚ * (x̄(λ) - x̄₀))   
-    η⁰(λ::AbstractFloat)  = (log.(L2.(λ) .^ 2))[1]
-    ρ⁰(λ::AbstractFloat)  = (log.(L1.(λ) .^ 2))[1]
+    η⁰(λ::AbstractFloat)  = (log.(L2.(λ) .^ 2.0))[1]
+    ρ⁰(λ::AbstractFloat)  = (log.(L1.(λ) .^ 2.0))[1]
+    ηᵖ(λ::AbstractFloat)  = (derivative(η⁰, λ))[1]
+    
+    function κ(λ::AbstractFloat)  
+        nᵖ = ηᵖ(λ)
+        η  = η⁰(λ)
+        ρ  = ρ⁰(λ)
+        return 1.0 - 2.0*η*ρ*(λ^2.0*nᵖ*ρ + 2.0*λ*η*ρ + λ^4.0*η*nᵖ) / 
+            (nᵖ * (λ^2.0 * η^2.0 + ρ^2.0)^1.5)
+    end
+    
+    return L1, L2, κ
+end 
+
+function Lcurve_functions(Ψ::RegularizationProblem, b̄::AbstractVector, )
+    x̄(λ::AbstractFloat)   = solve(Ψ, b̄, λ) 
+    L1(λ::AbstractFloat)  = norm(Ψ.Ā * x̄(λ)  - b̄)
+    L2(λ::AbstractFloat)  = norm(Ψ.Iₚ * (x̄(λ) - x̄₀))   
+    η⁰(λ::AbstractFloat)  = (log.(L2.(λ) .^ 2.0))[1]
+    ρ⁰(λ::AbstractFloat)  = (log.(L1.(λ) .^ 2.0))[1]
     ηᵖ(λ::AbstractFloat)  = (derivative(η⁰, λ))[1]
     
     function κ(λ::AbstractFloat)  
