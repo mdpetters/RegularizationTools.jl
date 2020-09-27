cff9dc6cc7e87830843e1188f9702cae74d4ac42
Markus Petters <mdpetter@ncsu.edu>
2020-09-27 18:54:17 -0400

    relax test and add new tests

--- test/solvers.jl
+++ test/solvers.jl
@@ -19,7 +19,7 @@ A, x = r.A, r.x
 y = A  * x
 b = y + 0.1y .* randn(100)
 
-xλ1 = @> setupRegularizationProblem(A,0) solve(b, alg=:L_curve, λ₁=100.0, λ₂=1e6) getfield(:x)
+xλ1 = @> setupRegularizationProblem(A,0) solve(b, alg=:L_curve, λ₁=10.0, λ₂=1000) getfield(:x)
 xλ2 = @> setupRegularizationProblem(A,0) solve(b, alg=:gcv_svd) getfield(:x)
 @test round(sum(xλ1), digits = 0) == round(sum(xλ2), digits = 0)
 
--- test/standard_form.jl
+++ test/standard_form.jl
@@ -15,40 +15,56 @@ L = Γ(A, 0)
 b̄, x̄₀ = to_standard_form(Ψ, b, x₀)
 sol1 = @>> solve(Ψ, b̄, x̄₀, λ) to_general_form(Ψ, b) 
 sol2 = inv(A'A + λ^2.0 * L'L) * (A' * b + λ^2.0 * L'L*x₀)
-@test sol1[1:n-1] ≈ sol2[1:n-1]
+@test sol1 ≈ sol2
 
 L = Γ(A, 1)
 Ψ = setupRegularizationProblem(A, L)
 b̄, x̄₀ = to_standard_form(Ψ, b, x₀)
 sol1 = @>> solve(Ψ, b̄, x̄₀, λ) to_general_form(Ψ, b) 
 sol2 = inv(A'A + λ^2.0 * L'L) * (A' * b + λ^2.0 * L'L*x₀)
-@test sol1[1:n-1] ≈ sol2[1:n-1]
+@test sol1 ≈ sol2
 
 L = Γ(A, 2)
 Ψ = setupRegularizationProblem(A, L)
 b̄, x̄₀ = to_standard_form(Ψ, b, x₀)
 sol1 = @>> solve(Ψ, b̄, x̄₀, λ) to_general_form(Ψ, b) 
 sol2 = inv(A'A + λ^2.0 * L'L) * (A' * b + λ^2.0 * L'L*x₀)
-@test sol1[1:n-1] ≈ sol2[1:n-1]
+@test sol1 ≈ sol2
+
+# Test reduced size L matrix
+L = Γ(A, 2)
+p, n = size(L)
+L = L[1:n-2, :]
+p, n = size(L)
+Ψ = setupRegularizationProblem(A, L)
+b̄, x̄₀ = to_standard_form(Ψ, b, x₀)
+sol1 = @>> solve(Ψ, b̄, x̄₀, λ) to_general_form(Ψ, b) 
+sol2 = inv(A'A + λ^2.0 * L'L) * (A' * b + λ^2.0 * L'L*x₀)
+@test sol1[1:p] ≈ sol2[1:p]
 
 
 L = Γ(A, 0)
+r = mdopen("shaw", 100, false)
+A, x = r.A, r.x
+Random.seed!(100) 
+
+L = rand(100,100)
 Ψ = setupRegularizationProblem(A, L)
 b̄ = to_standard_form(Ψ, b)
 sol1 = @>> solve(Ψ, b̄, λ) to_general_form(Ψ, b) 
 sol2 = inv(A'A + λ^2.0 * L'L) * A' * b 
-@test sol1[1:n-1] ≈ sol2[1:n-1]
+@test sol1 ≈ sol2
 
 L = Γ(A, 1)
 Ψ = setupRegularizationProblem(A, L)
 b̄ = to_standard_form(Ψ, b)
 sol1 = @>> solve(Ψ, b̄, λ) to_general_form(Ψ, b) 
 sol2 = inv(A'A + λ^2.0 * L'L) * A' * b 
-@test sol1[1:n-1] ≈ sol2[1:n-1]
+@test sol1 ≈ sol2
 
 L = Γ(A, 2)
 Ψ = setupRegularizationProblem(A, L)
 b̄ = to_standard_form(Ψ, b)
 sol1 = @>> solve(Ψ, b̄, λ) to_general_form(Ψ, b) 
 sol2 = inv(A'A + λ^2.0 * L'L) * A' * b 
-@test sol1[1:n-1] ≈ sol2[1:n-1]
+@test sol1 ≈ sol2
