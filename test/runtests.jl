ac23592a5acd6e60d5b84337016cc3f5a5de1a35
Markus Petters <mdpetter@ncsu.edu>
2020-12-08 16:22:53 -0500

    add invert tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,7 @@
 using RegularizationTools
 using MatrixDepot
+using Memoize
+using MLStyle
 using Random
 using Test
 using Underscores
@@ -10,5 +12,6 @@ using Lazy
     include("standard_form.jl")  # Test standard form transformation
     include("solvers.jl")        # Test optimization
     include("validators.jl")     # Test validators
-    include("domainfunctions.jl")
+    include("domainfunctions.jl") 
+    include("invert.jl")
 end
